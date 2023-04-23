20b4f252f655ff04cb539e6ac7567418a489e8db
arvirone <arvirone@wpi.edu>
2023-04-23 11:52:07 -0400

    update

--- src/main/java/edu/wpi/teame/controllers/HomePageController.java
+++ src/main/java/edu/wpi/teame/controllers/HomePageController.java
@@ -14,7 +14,6 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 
 public class HomePageController {
