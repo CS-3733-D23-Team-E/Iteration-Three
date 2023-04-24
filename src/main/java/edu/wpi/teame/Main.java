27dcfecbe4fe8f792e21df1dee2e887fdb846634
Kevin Siegall <digimonlord2@gmail.com>
2023-04-24 14:39:16 -0400

    Node and Edge changes

--- src/main/java/edu/wpi/teame/Main.java
+++ src/main/java/edu/wpi/teame/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
   public static void main(String[] args) {
     SQLRepo.INSTANCE.connectToDatabase("teame", "teame50");
-    SQLRepo.INSTANCE.getNodeList();
+    HospitalNode.processNodeList(SQLRepo.INSTANCE.getNodeList());
     HospitalNode.processEdgeList(SQLRepo.INSTANCE.getEdgeList());
     SQLRepo.INSTANCE.getLocationList();
     System.out.println(LocationName.allLocations.keySet());
