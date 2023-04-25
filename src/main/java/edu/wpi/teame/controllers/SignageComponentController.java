3e54aa47a6d17389e42dd43c735fd41b1bb5d1d7
Joseph Thesmar <thesmarjoseph@gmail.com>
2023-04-25 00:20:31 -0400

    started controller, merging with signageComponentDAO

--- src/main/java/edu/wpi/teame/controllers/SignageComponentController.java
+++ src/main/java/edu/wpi/teame/controllers/SignageComponentController.java
@@ -1,3 +1,22 @@
 package edu.wpi.teame.controllers;
 
-public class SignageComponentController {}
+import io.github.palexdev.materialfx.controls.MFXButton;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import org.controlsfx.control.SearchableComboBox;
+
+public class SignageComponentController {
+    @FXML MFXButton submitButton;
+    @FXML MFXButton cancelButton;
+    @FXML MFXButton resetButton;
+    @FXML DatePicker deliveryDateHere;
+    @FXML DatePicker deliveryDateUp;
+    @FXML DatePicker deliveryDateDown;
+    @FXML DatePicker deliveryDateLeft;
+    @FXML DatePicker deliveryDateRight;
+    @FXML SearchableComboBox<String> kioskLocationHere;
+    @FXML SearchableComboBox<String> kioskLocationUp;
+    @FXML SearchableComboBox<String> kioskLocationDown;
+    @FXML SearchableComboBox<String> kioskLocationLeft;
+    @FXML SearchableComboBox<String> kioskLocationRight;
+}
